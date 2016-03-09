0f6c568593e69c57c5a56b9c1af2a94d8acfba22
wisercoder <wisercoder@gmail.com>
2016-03-08 22:28:21 -0800

    bump version number

--- Demo/Views/Components/Product.tsx
+++ Demo/Views/Components/Product.tsx
@@ -1,4 +1,4 @@
-﻿/// <reference path="../../typings/uibuilder/uibuilder-1.0.d.ts" />
+﻿/// <reference path="../../typings/uibuilder/uibuilder-1.1.d.ts" />
 /// <reference path="../../Models/Product.ts" />
 
 module Demo.Views {
